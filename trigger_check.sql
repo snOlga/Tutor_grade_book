9a9a159aa5b3f88e6fe59165ef5998ee4dfb7bed
snOlga <olga.safonova@outlook.com>
2024-11-03 16:49:59 +0300

    still working on db

--- trigger_check.sql
+++ trigger_check.sql
@@ -5,3 +5,7 @@ SELECT * FROM students_lessons;
 UPDATE student_lessons_requests SET is_approved = TRUE WHERE lesson_id=3;
 
 SELECT * FROM students_lessons;
+
+INSERT INTO lessons (tutor_id, start_time, duration, subject_id, homework, is_open, is_active, description, human_readable_id) 
+VALUES 
+	(1, '2024-11-01 10:00:00', '01:30:00', 1, 'Solve problems', TRUE, TRUE, 'Mathematics lesson', 'lesson-01');
--- triggers.sql
+++ triggers.sql
@@ -41,3 +41,48 @@ AFTER UPDATE OF is_approved ON student_lessons_requests
 FOR EACH ROW
 WHEN (NEW.is_approved = TRUE)
 EXECUTE FUNCTION update_student_lesson_from_student();
+
+
+
+CREATE OR REPLACE FUNCTION insert_lesson_if_not_exists()
+RETURNS TRIGGER AS $$
+	BEGIN
+		IF EXISTS (
+			SELECT * FROM lessons WHERE tutor_id = NEW.tutor_id AND 
+				(((start_time + duration) > NEW.start_time) AND 
+					(start_time < NEW.start_time) OR 
+					((start_time + duration) > (NEW.start_time + duration)) AND
+					(start_time >= NEW.start_time))
+			)
+		THEN
+			RAISE EXCEPTION 'At this time lesson already exists';
+		END IF;
+		RETURN NEW;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER before_insert_lesson_if_not_exists
+BEFORE INSERT
+ON lessons
+FOR EACH ROW
+EXECUTE FUNCTION insert_lesson_if_not_exists();
+
+
+
+CREATE OR REPLACE FUNCTION delete_lesson()
+RETURNS TRIGGER AS $$
+	BEGIN
+		UPDATE lessons SET is_active = FALSE WHERE ID = OLD.ID;
+		RETURN NULL;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER before_delete_lesson
+BEFORE DELETE
+ON lessons
+FOR EACH ROW
+EXECUTE FUNCTION delete_lesson();
+
+
+
+
