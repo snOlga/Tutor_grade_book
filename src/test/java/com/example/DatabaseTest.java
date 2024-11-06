54ee4a3758623e713426dda0d1264872635bccdd
CHB_0r1s <borisluzin2004@gmail.com>
2024-11-06 05:18:04 +0300

    add tests template

--- src/test/java/com/example/DatabaseTest.java
+++ src/test/java/com/example/DatabaseTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-class CustomerServiceTest {
+class DatabaseTest {
 
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
             "postgres:16-alpine"
