484791f0122679625e32e1d6e536496243d5255b
CHB_0r1s <borisluzin2004@gmail.com>
2024-11-06 12:19:56 +0300

    Fix triggers

--- full_script.sql
+++ full_script.sql
@@ -130,11 +130,11 @@ CREATE TRIGGER after_approving_lesson_by_student
 AFTER UPDATE OF is_approved ON tutor_lessons_requests
 FOR EACH ROW
 WHEN (NEW.is_approved = TRUE)
-EXECUTE FUNCTION update_student_lesson_from_tutor();
+EXECUTE FUNCTION update_student_lessons_from_tutor();
 
 
 
-CREATE OR REPLACE FUNCTION update_student_lesson_from_student()
+CREATE OR REPLACE FUNCTION update_student_lessons_from_student()
 RETURNS TRIGGER AS $$
 	BEGIN
 		IF NOT EXISTS (
@@ -153,7 +153,7 @@ CREATE TRIGGER after_approving_lesson_by_tutor
 AFTER UPDATE OF is_approved ON student_lessons_requests
 FOR EACH ROW
 WHEN (NEW.is_approved = TRUE)
-EXECUTE FUNCTION update_student_lesson_from_student();
+EXECUTE FUNCTION update_student_lessons_from_student();
 
 
 
